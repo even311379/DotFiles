78841f078d0c68b79f5ba465ffb4aebf70667b75
even311379 <even311379@hotmail.com>
2023-11-09 22:10:04 +0800

    update autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -11,8 +11,19 @@ numlockx on &
 
 picom --config /home/even/.config/picom/picom.conf -b &
 
-xrandr --output HDMI-0 --mode 1920x1080 --pos 3840x0 --filter bilinear --scale-from 2560x1440 &
+# xrandr --output HDMI-0 --mode 1920x1080 --pos 3840x0 --filter bilinear --scale-from 2560x1440 &
 
 fcitx5 &
 
-nitrogen --restore &
+systemctl --user restart dunst.service &
+# nitrogen --restore &
+# linux-wallpaperengine --screen-root DP-0 2872388690 --screen-root HDMI-0 1201340849 &
+# linux-wallpaperengine --screen-root DP-0 --screen-root HDMI-0 1201340849 &
+
+killall conky
+
+sleep 10s && conky -c "/home/even/.config/qtile/qtile.conkyrc" &
+
+# export XDG_CURRENT_DESKTOP=KDE
+# export XDG_SESSION_DESKTOP=KDE
+# export XDG_SESSION_VERSION=5
